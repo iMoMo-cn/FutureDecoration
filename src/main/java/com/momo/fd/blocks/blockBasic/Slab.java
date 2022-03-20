bd7af5fb51f66c90c7a503e371b3d467cb3c98b8
MoMo渣 <61381841+iMoMo-cn@users.noreply.github.com>
2022-03-21 01:31:36 +0800

    fix slab

--- src/main/java/com/momo/fd/blocks/blockBasic/Slab.java
+++ src/main/java/com/momo/fd/blocks/blockBasic/Slab.java
@@ -18,9 +18,12 @@ import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
+import net.minecraft.item.ItemSlab;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.IStringSerializable;
+import net.minecraft.util.NonNullList;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
@@ -30,16 +33,15 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 import java.util.Random;
 
 public class Slab extends BlockSlab implements IHasModel {
-    public static final PropertyEnum<Slab.Variant> VARIANT = PropertyEnum.<Slab.Variant>create("variant", Slab.Variant.class);
     public Block dropBlock;
 
     public Slab(String name, MapColor mapColor, Block block)
     {
         super(Material.ROCK, mapColor);
-        IBlockState iblockstate = this.blockState.getBaseState();
         this.setResistance(10.0F);
         this.setHardness(2.0F);
         this.setSoundType(SoundType.STONE);
+
         this.setUnlocalizedName(name);
         this.setRegistryName(name);
 
@@ -48,154 +50,54 @@ public class Slab extends BlockSlab implements IHasModel {
         ModBlocks.BLOCKS.add(this);
         ModItems.ITEMS.add(new ItemBlock(this).setRegistryName(this.getRegistryName()));
 
-        if (!this.isDouble())
-        {
-            iblockstate = iblockstate.withProperty(HALF, BlockSlab.EnumBlockHalf.BOTTOM);
-        }
+        IBlockState iblockstate = this.blockState.getBaseState();
+        if (!this.isDouble()) { iblockstate = iblockstate.withProperty(HALF, EnumBlockHalf.BOTTOM); }
 
-        this.setDefaultState(iblockstate.withProperty(VARIANT, Slab.Variant.DEFAULT));
+        this.setDefaultState(iblockstate);
         this.setCreativeTab(CreativeTabs.BUILDING_BLOCKS);
+
+        useNeighborBrightness = true;
     }
 
     @Override
-    public void registerModels() {
-        MoMoFramework.proxy.registerItemRenderer(Item.getItemFromBlock(this), 0, "inventory");
-    }
+    public void registerModels() { MoMoFramework.proxy.registerItemRenderer(Item.getItemFromBlock(this), 0, "inventory"); }
 
-    /**
-     * Convert the given metadata into a BlockState for this Block
-     */
-    public IBlockState getStateFromMeta(int meta)
-    {
-        IBlockState iblockstate = this.getDefaultState().withProperty(VARIANT, Slab.Variant.DEFAULT);
+    @Override
+    protected BlockStateContainer createBlockState() { return new BlockStateContainer(this, HALF); }
 
-        if (!this.isDouble())
-        {
-            iblockstate = iblockstate.withProperty(HALF, (meta & 8) == 0 ? BlockSlab.EnumBlockHalf.BOTTOM : BlockSlab.EnumBlockHalf.TOP);
-        }
-
-        return iblockstate;
-    }
-
-    /**
-     * Convert the BlockState into the correct metadata value
-     */
-    public int getMetaFromState(IBlockState state)
-    {
-        int i = 0;
-
-        if (!this.isDouble() && state.getValue(HALF) == BlockSlab.EnumBlockHalf.TOP)
-        {
-            i |= 8;
-        }
-
-        return i;
-    }
-
-    protected BlockStateContainer createBlockState()
-    {
-        return this.isDouble() ? new BlockStateContainer(this, new IProperty[] {VARIANT}) : new BlockStateContainer(this, new IProperty[] {HALF, VARIANT});
-    }
-
-    /**
-     * Returns the slab block name with the type associated with it
-     */
     public String getUnlocalizedName(int meta)
     {
         return super.getUnlocalizedName();
     }
 
-    @SideOnly(Side.CLIENT)
-    public boolean shouldSideBeRendered(IBlockState blockState, IBlockAccess blockAccess, BlockPos pos, EnumFacing side)
+    public IProperty<?> getVariantProperty()
     {
-        if (this.isDouble())
-        {
-            return super.shouldSideBeRendered(blockState, blockAccess, pos, side);
-        }
-        else if (side != EnumFacing.UP && side != EnumFacing.DOWN && !super.shouldSideBeRendered(blockState, blockAccess, pos, side))
-        {
-            return false;
-        }
-        else if (false) // Forge: Additional logic breaks doesSideBlockRendering and is no longer useful.
-        {
-            IBlockState iblockstate = blockAccess.getBlockState(pos.offset(side));
-            boolean flag = isHalfSlab(iblockstate) && iblockstate.getValue(HALF) == BlockSlab.EnumBlockHalf.TOP;
-            boolean flag1 = isHalfSlab(blockState) && blockState.getValue(HALF) == BlockSlab.EnumBlockHalf.TOP;
-
-            if (flag1)
-            {
-                if (side == EnumFacing.DOWN)
-                {
-                    return true;
-                }
-                else if (side == EnumFacing.UP && super.shouldSideBeRendered(blockState, blockAccess, pos, side))
-                {
-                    return true;
-                }
-                else
-                {
-                    return !isHalfSlab(iblockstate) || !flag;
-                }
-            }
-            else if (side == EnumFacing.UP)
-            {
-                return true;
-            }
-            else if (side == EnumFacing.DOWN && super.shouldSideBeRendered(blockState, blockAccess, pos, side))
-            {
-                return true;
-            }
-            else
-            {
-                return !isHalfSlab(iblockstate) || flag;
-            }
-        }
-        return super.shouldSideBeRendered(blockState, blockAccess, pos, side);
+        return HALF;
     }
-
-    @SideOnly(Side.CLIENT)
-    protected static boolean isHalfSlab(IBlockState state) { return true; }
-
-    public Item getItemDropped(IBlockState state, Random rand, int fortune) { return Item.getItemFromBlock(dropBlock); }
-
-    public ItemStack getItem(World worldIn, BlockPos pos, IBlockState state) { return new ItemStack(dropBlock); }
 
     @Override
     public boolean isDouble() { return false; }
 
-    public IProperty<?> getVariantProperty()
-    {
-        return VARIANT;
-    }
-
+    @Override
     public Comparable<?> getTypeForItem(ItemStack stack)
     {
-        return Slab.Variant.DEFAULT;
+        return EnumBlockHalf.BOTTOM;
     }
 
-//    public static class Double extends Slab
-//    {
-//        public boolean isDouble()
-//        {
-//            return true;
-//        }
-//    }
-//
-//    public static class Half extends Slab
-//    {
-//        public boolean isDouble()
-//        {
-//            return false;
-//        }
-//    }
+    public int getMetaFromState(IBlockState state) { return isDouble() ? 0 : state.getValue(HALF).ordinal() + 1; }
 
-    public static enum Variant implements IStringSerializable
+    @Override
+    public IBlockState getStateFromMeta(int meta)
     {
-        DEFAULT;
+        return !isDouble() ? getDefaultState().withProperty(HALF, EnumBlockHalf.values()[meta % EnumBlockHalf.values().length]) : getDefaultState();
+    }
 
-        public String getName()
-        {
-            return "default";
-        }
+    @Override
+    public void getDrops(NonNullList<ItemStack> drops, IBlockAccess world, BlockPos pos, IBlockState state, int fortune)
+    {
+        if(isDouble())
+            drops.add(new ItemStack(dropBlock, 2));
+        else
+            drops.add(new ItemStack(dropBlock, 1));
     }
 }
