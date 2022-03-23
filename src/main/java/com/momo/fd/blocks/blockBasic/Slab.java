5d47b939a65f91ece5b379919c97d73b069677d3
MoMo渣 <61381841+iMoMo-cn@users.noreply.github.com>
2022-03-23 13:58:02 +0800

    Update Slab.java

--- src/main/java/com/momo/fd/blocks/blockBasic/Slab.java
+++ src/main/java/com/momo/fd/blocks/blockBasic/Slab.java
@@ -15,6 +15,7 @@ import net.minecraft.block.properties.PropertyEnum;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
@@ -25,6 +26,7 @@ import net.minecraft.util.EnumFacing;
 import net.minecraft.util.IStringSerializable;
 import net.minecraft.util.NonNullList;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
@@ -115,9 +117,14 @@ public class Slab extends BlockSlab implements IHasModel, IItemProvider {
         return !isDouble() ? getDefaultState().withProperty(HALF, EnumBlockHalf.values()[meta % EnumBlockHalf.values().length]) : getDefaultState();
     }
 
+    public ItemStack getPickBlock(IBlockState state, RayTraceResult target, World world, BlockPos pos, EntityPlayer player)
+    {
+        return getItem(world, pos, state);
+    }
+
     public ItemStack getItem(World worldIn, BlockPos pos, IBlockState state)
     {
-        return new ItemStack(dropBlock);
+        return new ItemStack(dropBlock, 1, dropBlock.damageDropped(state));
     }
 
     @Override
