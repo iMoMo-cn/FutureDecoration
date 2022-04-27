d39c9195ac434a0712cd6f016c485e933d50b3ae
MoMo渣 <61381841+iMoMo-cn@users.noreply.github.com>
2022-04-27 20:02:29 +0800

    fix golden door drop

--- src/main/java/com/momo/fd/blocks/blockInteractive/Door.java
+++ src/main/java/com/momo/fd/blocks/blockInteractive/Door.java
@@ -6,11 +6,14 @@ import net.minecraft.block.BlockDoor;
 import net.minecraft.block.material.MapColor;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
+
+import java.util.Random;
 
 public class Door extends BlockDoor implements IHasModel {
 
@@ -39,6 +42,12 @@ public class Door extends BlockDoor implements IHasModel {
     public ItemStack getItem(World worldIn, BlockPos pos, IBlockState state)
     {
         return new ItemStack(itemDoor);
+    }
+
+    @Override
+    public Item getItemDropped(IBlockState state, Random rand, int fortune)
+    {
+        return state.getValue(HALF) == BlockDoor.EnumDoorHalf.UPPER ? Items.AIR : this.itemDoor;
     }
 
     @Override
